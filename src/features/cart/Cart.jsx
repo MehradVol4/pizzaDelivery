f0c9d9e00c3f4832492581cb7254f186f62e96eb
Mehrad Khademi <mehradkhd.vol4@gmail.com>
2026-05-06 00:43:11 +0330

    add all styles

--- src/features/cart/Cart.jsx
+++ src/features/cart/Cart.jsx
@@ -2,44 +2,49 @@
 import Username from "../user/Username";
 import LinkButton from '../../ui/LinkButton';
 import Button from "../../ui/Button";
-// const fakeCart = [
-//   {
-//     pizzaId: 12,
-//     name: 'Mediterranean',
-//     quantity: 2,
-//     unitPrice: 16,
-//     totalPrice: 32,
-//   },
-//   {
-//     pizzaId: 6,
-//     name: 'Vegetale',
-//     quantity: 1,
-//     unitPrice: 13,
-//     totalPrice: 13,
-//   },
-//   {
-//     pizzaId: 11,
-//     name: 'Spinach and Mushroom',
-//     quantity: 1,
-//     unitPrice: 15,
-//     totalPrice: 15,
-//   },
-// ];
+import CartItem from "./CartItem";
+
+const fakeCart = [
+  {
+    pizzaId: 12,
+    name: 'Mediterranean',
+    quantity: 2,
+    unitPrice: 16,
+    totalPrice: 32,
+  },
+  {
+    pizzaId: 6,
+    name: 'Vegetale',
+    quantity: 1,
+    unitPrice: 13,
+    totalPrice: 13,
+  },
+  {
+    pizzaId: 11,
+    name: 'Spinach and Mushroom',
+    quantity: 1,
+    unitPrice: 15,
+    totalPrice: 15,
+  },
+];
 
 function Cart() {
-  // const cart = fakeCart;
+  const cart = fakeCart;
 
   return (
-    <div>
+    <div className="py-3 px-4">
 
       <LinkButton to="/menu">
         &larr; Back to menu
       </LinkButton>
 
-      <h2>Your cart, {Username}</h2>
-
-      <div>
+      <h2 className="mt-7 text-xl font-semibold">Your cart, {Username}</h2>
+      <ul className="divide-y divide-stone-200 border-b mt-3 ">
+        {cart.map((item) => <CartItem item={item} key={item.key} />)}
+      </ul>
+      <div className="mt-6 space-x-2 ">
         <Button to="/order/new" type="primary">Order pizzas</Button>
+        <Button type="secondary">clear cart</Button>
         <button>Clear cart</button>
       </div>
     </div>
--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -1,10 +1,11 @@
 import { Link } from "react-router-dom"
 
 function Button({ children, disabled, to, type }) {
-    const base = "bg-yellow-400 uppercase font-semibold text-stone-800 inline-block tracking-wide rounded-xl hover:bg-yellow-300 cursor-pointer transition-all focus:outline-none disabled:cursor-not-allowed " ;
+    const base = "bg-yellow-400 text-sm uppercase font-semibold text-stone-800 inline-block tracking-wide rounded-xl hover:bg-yellow-300 cursor-pointer transition-all focus:outline-none disabled:cursor-not-allowed " ;
     const styles = {
         primary : base + "px-4 py-3 md:px-6 md:py-4" ,
         small : base + "py-2 px-4 md:px-5 md:py-2.5 text-xs" ,
+        secondary : "inline-block text-sm rounded-full bg-transparent font-semibold uppercasse tracking-wide text-stone-800 transition-colors duration-300 hover:bg-yellow-300 focus:bg-yellow-300 focus:ring focus:ring-yelllow-300 focus:ring-offset-2 disable:cursor-not-allowed border-2 border-stone-300 px-4 py-3 md:px-6 md:py-6"
     }
     if (to) return (
         <Link
